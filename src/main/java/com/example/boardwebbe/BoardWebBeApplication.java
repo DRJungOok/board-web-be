f22f59322294900c9281a4249239646996d3e274
DRJungOok <DRJungOok@gmail.com>
2024-03-03 02:19:48 +0900

    set board-web #1

---
+++ src/main/java/com/example/boardwebbe/BoardWebBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardwebbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardWebBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardWebBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/boardwebbe/BoardWebBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boardwebbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardWebBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
